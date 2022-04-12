89e190730a951367b812430dfeba22cf0284b8f1
madhav <madhav@gamil.com>
2022-04-12 17:04:35 +0530

    initial comit

---
+++ index.php
@@ -0,0 +1,157 @@
+﻿<!DOCTYPE html>
+
+
+<head>
+	
+<meta charset="utf-8">
+	
+<meta name="viewport" content="width=devidev-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Online Ticket Reservation</title>
+	
+	
+
+<!-- [ PLUGIN STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+<link rel="shortcut icon" type="image/x-icon" href="images/icon.png">
+	
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+	
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+        
+<link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+	
+<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+	
+<!-- [ Boot STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
+       
+<!-- [ DEFAULT STYLESHEET ] 
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="css/style.css">
+        
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	
+<link rel="stylesheet" type="text/css" href="css/color/rose.css">
+        
+
+</head>
+<body >
+
+
+
+=============================================================================================================================-->
+
+<!-- [WRAPPER ]
+
+=============================================================================================================================-->
+
+<div class="wrapper">
+  
+<!-- [NAV]
+ 
+============================================================================================================================-->    
+   
+<!-- Navigation
+    ==========================================-->
+    
+<nav  class=" nim-menu navbar navbar-default navbar-fixed-top">
+      
+<div class="container">
+        
+<!-- Brand and toggle get grouped for better mobile display -->
+        
+<div class="navbar-header">
+          
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            
+<span class="sr-only">Toggle navigation</span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+          
+</button>
+            
+<a class="navbar-brand" href="index.html">Travel Ticket Booking</a>
+        
+</div>
+        
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          
+<ul class="nav navbar-nav navbar-right">
+            
+<li><a href="#home" ><h3>Home</h3></a></li>
+            
+<li><a href="#two" ><h3>About</h3></a></li>
+            
+<li><a href="#four" ><h3>Contact</h3></a></li>
+
+<li><a href="admin/" ><h3>Login</h3></a></li>
+          
+</ul>
+        
+</div>
+
+      
+</div>
+    
+</nav>
+
+
+   
+ 
+    
+   
+
+ 
+ 
+   
+<section class="main-heading" id="home">
+       
+<div class="overlay">
+           
+<div class="container">
+               
+<div class="row">
+                   
+<div class="main-heading-content col-md-12 col-sm-12 text-center">
+        
+<h1 class="main-heading-title">Online Ticket</h1>
+ 
+<h1 class="main-heading-title">Reservation System</h1>
+       
+<p class="main-heading-text">WELCOME TO,<br/>TRAVEL TICKET BOOKING SYSTEM</p>
+        
+<div class="btn-bar">
+          
+<a href="reserved.php" class="btn btn-primary">Reserve Now</a>
+                 
+</div>
+      
+</div>
+               
+</div>
+           
+</div>
+       
+</div>  
+	
+
+  
+</body>
+
+
+</html>
